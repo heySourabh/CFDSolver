5906f657d6ccbd5d0301ee8cdfe9d1b20b6a5303
Sourabh Bhat <heySourabh@gmail.com>
2018-04-15 16:05:20 +0530

    Implemented the Triangle Geometry

---
+++ src/main/geom/factory/Triangle.java
@@ -0,0 +1,45 @@
+package main.geom.factory;
+
+import main.geom.Geometry;
+import main.geom.Point;
+import main.geom.VTKType;
+import main.geom.Vector;
+
+public class Triangle implements Geometry {
+    private final Point[] points;
+    private final VTKType vtkType;
+    private final double area;
+
+    public Triangle(Point p1, Point p2, Point p3) {
+        this.points = new Point[]{p1, p2, p3};
+        this.vtkType = VTKType.VTK_TRIANGLE;
+        Vector v1 = new Vector(p1, p2);
+        Vector v2 = new Vector(p1, p3);
+        area = v1.cross(v2).mag() * 0.5;
+    }
+
+    @Override
+    public Point[] points() {
+        return points;
+    }
+
+    @Override
+    public VTKType vtkType() {
+        return vtkType;
+    }
+
+    @Override
+    public double length() {
+        throw new ArithmeticException("Cannot calculate length of a triangle.");
+    }
+
+    @Override
+    public double area() {
+        return area;
+    }
+
+    @Override
+    public double volume() {
+        throw new ArithmeticException("Cannot calculate volume of a triangle.");
+    }
+}
