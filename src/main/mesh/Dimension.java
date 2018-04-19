27dc3ed3c870643fc868af518c481cca159a51a0
Sourabh Bhat <heySourabh@gmail.com>
2018-04-19 17:58:36 +0530

    Added helper class for reading mesh files and parameters Modified Dimension enum

--- src/main/mesh/Dimension.java
+++ src/main/mesh/Dimension.java
@@ -1,5 +1,24 @@
 package main.mesh;
 
 public enum Dimension {
-    ONE_DIM, TWO_DIM, THREE_DIM
+    ONE_DIM(1), TWO_DIM(2), THREE_DIM(3);
+
+    public final int dim;
+
+    Dimension(int dim) {
+        this.dim = dim;
+    }
+
+    public static Dimension getDimension(int dim) {
+        switch (dim) {
+            case 1:
+                return ONE_DIM;
+            case 2:
+                return TWO_DIM;
+            case 3:
+                return THREE_DIM;
+            default:
+                throw new IllegalArgumentException("No enum for dim " + dim);
+        }
+    }
 }
---
+++ src/main/util/MeshFileReader.java
@@ -0,0 +1,50 @@
+package main.util;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MeshFileReader implements Closeable {
+
+    private final Scanner fileScanner;
+    private final String commentStr;
+
+    public MeshFileReader(File file) throws FileNotFoundException {
+        this(file, null);
+    }
+
+    public MeshFileReader(File file, String commentStr) throws FileNotFoundException {
+        this.fileScanner = new Scanner(file);
+        this.commentStr = commentStr;
+    }
+
+    public void close() {
+        fileScanner.close();
+    }
+
+    /**
+     * Read the next line from the file ignoring blank lines and text after comment string.
+     *
+     * @return Next valid line.
+     */
+    public String nextLine() {
+        String nextLine = fileScanner.nextLine().trim();
+
+        if (commentStr != null)
+            nextLine = nextLine.split(commentStr)[0].trim();
+
+        // Since blank lines are ignored
+        if (nextLine.length() == 0)
+            nextLine = nextLine();
+
+        return nextLine;
+    }
+
+    public String readParameter(String param) {
+        String[] tokens = nextLine().split("=");
+        if (!tokens[0].trim().equals(param))
+            throw new IllegalArgumentException("The expected parameter " + param + " does not exist at the location.");
+        return tokens[1].trim();
+    }
+}
