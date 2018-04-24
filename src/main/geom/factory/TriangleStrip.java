2613cea9dbc13e55a61b8d3c2ddca40fc85e60f8
Sourabh Bhat <heySourabh@gmail.com>
2018-04-24 08:51:19 +0530

    Added implementation of area for TriangleStrip class

--- src/main/geom/factory/TriangleStrip.java
+++ src/main/geom/factory/TriangleStrip.java
@@ -4,16 +4,22 @@ import main.geom.Geometry;
 import main.geom.Point;
 import main.geom.VTKType;
 
+import java.util.stream.IntStream;
+
 public class TriangleStrip implements Geometry {
 
     private final Point[] points;
     private final VTKType vtkType;
     private final double area;
 
-    public TriangleStrip(Point[] points) {
+    public TriangleStrip(Point... points) {
         this.points = points;
         this.vtkType = VTKType.VTK_TRIANGLE_STRIP;
-        this.area = Double.NaN;
+
+        this.area = IntStream.range(0, points.length - 2)
+                .mapToObj(i -> new Triangle(points[i], points[i + 1], points[i + 2]))
+                .mapToDouble(Triangle::area)
+                .sum();
     }
 
     @Override
@@ -33,7 +39,7 @@ public class TriangleStrip implements Geometry {
 
     @Override
     public double area() {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return area;
     }
 
     @Override
