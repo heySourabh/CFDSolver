19d958de3e0c1ba13fd11dbbd91f059b9a78313a
Sourabh Bhat <heySourabh@gmail.com>
2018-05-25 17:45:07 +0530

    Adding simple implementation of BoundaryCondition -> ExtrapolatedBC

---
+++ src/main/physics/bc/ExtrapolatedBC.java
@@ -0,0 +1,27 @@
+package main.physics.bc;
+
+import main.mesh.Cell;
+import main.mesh.Face;
+import main.physics.goveqn.GoverningEquations;
+import main.util.DoubleArray;
+
+public class ExtrapolatedBC implements BoundaryCondition {
+    private final GoverningEquations govEqn;
+
+    public ExtrapolatedBC(GoverningEquations govEqn) {
+        this.govEqn = govEqn;
+    }
+
+    @Override
+    public void setGhostCellValues(Face face, double time) {
+        Cell outerCell = face.right;
+        Cell innerCell = face.left;
+        DoubleArray.copy(innerCell.U, outerCell.U);
+    }
+
+    @Override
+    public double[] convectiveFlux(Face face, double time) {
+        double[] innerVars = face.left.U;
+        return govEqn.convection().flux(innerVars, face.surface.unitNormal);
+    }
+}
