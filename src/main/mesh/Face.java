489b78911e90d4738f8eedafe107810998c199e5
Sourabh Bhat <heySourabh@gmail.com>
2018-06-25 18:05:25 +0530

    Added hashCode() and equals() to Face  - The methods will automate the process of removal of duplicates in    a Set and setting of the neighbors

--- src/main/mesh/Face.java
+++ src/main/mesh/Face.java
@@ -2,11 +2,15 @@ package main.mesh;
 
 import main.geom.VTKType;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Face {
     public final Node[] nodes;
     public final VTKType vtkType;
     public final Surface surface;
-    public final Cell left, right;
+    public final Cell left;
+    public Cell right;
 
     public final double[] flux;
 
@@ -16,9 +20,43 @@ public class Face {
         this.nodes = nodes;
         this.vtkType = vtkType;
         this.surface = surface;
+
+        if (left == null) throw new IllegalArgumentException("The left neighbor cannot be null");
         this.left = left;
         this.right = right;
 
         this.flux = new double[numVars];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Face otherFace = (Face) o;
+        Face thisFace = this;
+
+        boolean same = hasSameNodes(thisFace.nodes, otherFace.nodes);
+        if (same) {
+            thisFace.right = otherFace.left;
+            otherFace.right = thisFace.left;
+        }
+
+        return same;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.stream(this.nodes)
+                .mapToInt(Object::hashCode)
+                .sum();
+    }
+
+    private boolean hasSameNodes(Node[] a1, Node[] a2) {
+        List<Node> l1 = Arrays.asList(a1);
+        List<Node> l2 = Arrays.asList(a2);
+
+        if (l1.size() != l2.size()) return false;
+        return l1.containsAll(l2);
     }
 }
