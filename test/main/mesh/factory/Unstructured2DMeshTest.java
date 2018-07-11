41f23d3684f3e47ef6b330610ab2405c68facad6
Sourabh Bhat <heySourabh@gmail.com>
2018-07-11 18:16:14 +0530

    Optimized imports

--- test/main/mesh/factory/Unstructured2DMeshTest.java
+++ test/main/mesh/factory/Unstructured2DMeshTest.java
@@ -21,9 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.toList;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class Unstructured2DMeshTest {
 
