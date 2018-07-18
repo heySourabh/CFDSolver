6db5bacee8d2856d68420a1d86898326480a375c
Sourabh Bhat <heySourabh@gmail.com>
2018-07-18 18:05:55 +0530

    Added Mach number method to EulerEquations

--- src/main/physics/goveqn/factory/EulerEquations.java
+++ src/main/physics/goveqn/factory/EulerEquations.java
@@ -24,6 +24,19 @@ public class EulerEquations implements GoverningEquations {
         return p / rho / R;
     }
 
+    public double mach(double[] primitiveVars) {
+        double rho = primitiveVars[0];
+        double u = primitiveVars[1];
+        double v = primitiveVars[2];
+        double w = primitiveVars[3];
+        double p = primitiveVars[4];
+
+        double flowVelocity = new Vector(u, v, w).mag();
+        double soundSpeed = Math.sqrt(GAMMA * p / rho);
+
+        return flowVelocity / soundSpeed;
+    }
+
     @Override
     public String description() {
         return "Euler Equations (Inviscid fluid flow governing equations)";
