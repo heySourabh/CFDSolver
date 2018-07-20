506e92cff17742757b1aa30def49d5659f2926ea
Sourabh Bhat <heySourabh@gmail.com>
2018-07-20 15:30:32 +0530

    Diffusion argument changed to array of Vector for gradients

--- src/main/physics/goveqn/Diffusion.java
+++ src/main/physics/goveqn/Diffusion.java
@@ -3,5 +3,5 @@ package main.physics.goveqn;
 import main.geom.Vector;
 
 public interface Diffusion {
-    double[] flux(double[] conservativeVars, double[] gradConservativeVars, Vector unitNormal);
+    double[] flux(double[] conservativeVars, Vector[] gradConservativeVars, Vector unitNormal);
 }
