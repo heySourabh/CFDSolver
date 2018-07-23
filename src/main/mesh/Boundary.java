24771fb1a55504b0f19c23bd75a735ad07d94838
Sourabh Bhat <heySourabh@gmail.com>
2018-07-23 18:14:50 +0530

    Added a method to set boundary condition after construction

--- src/main/mesh/Boundary.java
+++ src/main/mesh/Boundary.java
@@ -7,11 +7,15 @@ import java.util.List;
 public class Boundary {
     public final String name;
     public final List<Face> faces;
-    public final BoundaryCondition bc;
+    public BoundaryCondition bc;
 
     public Boundary(String name, List<Face> faces, BoundaryCondition bc) {
         this.name = name;
         this.faces = faces;
+        this.bc = bc;
+    }
+
+    public void setBC(BoundaryCondition bc) {
         this.bc = bc;
     }
 }
