40ff6e4bbb84f92ea6666ae42839fbd9f031c33d
Sourabh Bhat <heySourabh@gmail.com>
2018-07-28 07:58:27 +0530

    Minor refactoring

--- test/main/solver/reconstructor/FaceNeighborsTest.java
+++ test/main/solver/reconstructor/FaceNeighborsTest.java
@@ -18,14 +18,14 @@ public class FaceNeighborsTest {
     public void get() throws FileNotFoundException {
         Mesh mesh = new Unstructured2DMesh(
                 new File("test/test_data/mesh_unstructured_2d.cfdu"), 5, Map.of());
-        Neighbors faceNeighbors = new FaceNeighbors();
 
         Cell c0 = mesh.cells().get(0);
         Cell c1 = mesh.cells().get(1);
         Cell c2 = mesh.cells().get(2);
         Cell c5 = mesh.cells().get(5);
         List<Cell> expectedNeighs = List.of(c0, c2, c5);
-        List<Cell> actualNeighs = faceNeighbors.getFor(c1);
+
+        List<Cell> actualNeighs = new FaceNeighbors().getFor(c1);
 
         assertEquals(expectedNeighs.size(), actualNeighs.size());
         assertTrue(expectedNeighs.containsAll(actualNeighs));
