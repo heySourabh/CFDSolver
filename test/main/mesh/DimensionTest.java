051e414726608ececdab6fc01384e6c8323e4684
Sourabh Bhat <heySourabh@gmail.com>
2018-08-03 02:09:48 +0530

    Using assertThrows() from TestHelper

--- test/main/mesh/DimensionTest.java
+++ test/main/mesh/DimensionTest.java
@@ -2,10 +2,9 @@ package main.mesh;
 
 import org.junit.Test;
 
-import static main.mesh.Dimension.ONE_DIM;
-import static main.mesh.Dimension.THREE_DIM;
-import static main.mesh.Dimension.TWO_DIM;
-import static org.junit.Assert.*;
+import static main.TestHelper.assertThrows;
+import static main.mesh.Dimension.*;
+import static org.junit.Assert.assertEquals;
 
 public class DimensionTest {
 
@@ -23,11 +22,6 @@ public class DimensionTest {
         assertEquals(ONE_DIM, Dimension.getDimension(1));
         assertEquals(TWO_DIM, Dimension.getDimension(2));
         assertEquals(THREE_DIM, Dimension.getDimension(3));
-        try {
-            assertEquals(THREE_DIM, Dimension.getDimension(4));
-            fail("Exception not thrown as expected.");
-        } catch (IllegalArgumentException e) {
-            // exception thrown as expected
-        }
+        assertThrows(IllegalArgumentException.class, () -> Dimension.getDimension(4));
     }
 }
