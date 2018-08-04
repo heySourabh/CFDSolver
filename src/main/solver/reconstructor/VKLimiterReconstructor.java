8366d8d687e1131e2080f380adc560094d0b26c0
Sourabh Bhat <heySourabh@gmail.com>
2018-08-04 12:15:13 +0530

    Implemented basic Venkatakrishnan limiter with K=0, test for VK solver.

---
+++ src/main/solver/reconstructor/VKLimiterReconstructor.java
@@ -0,0 +1,151 @@
+package main.solver.reconstructor;
+
+import main.geom.Point;
+import main.geom.Vector;
+import main.mesh.Cell;
+import main.mesh.Mesh;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static main.util.DoubleArray.add;
+import static main.util.DoubleArray.*;
+import static main.util.DoubleArray.multiply;
+import static main.util.DoubleMatrix.*;
+import static main.util.DoubleMatrix.multiply;
+
+public class VKLimiterReconstructor implements SolutionReconstructor {
+    private final Cell[][] neighbors;
+    private final double[][][] matrices;
+    private final NeighborsCalculator neighsCalc = new FaceNeighbors();
+    private final Mesh mesh;
+
+    public VKLimiterReconstructor(Mesh mesh) {
+        this.mesh = mesh;
+        final int numCells = mesh.cells().size();
+        neighbors = new Cell[numCells][];
+        matrices = new double[numCells][][];
+        mesh.cellStream().forEach(this::setup);
+    }
+
+    private void setup(Cell cell) {
+        Cell[] neighs = neighsCalc.getFor(cell).toArray(new Cell[0]);
+        neighbors[cell.index] = neighs;
+
+        List<Vector> distanceVectors = Arrays.stream(neighs)
+                .map(neighCell -> new Vector(cell.shape.centroid, neighCell.shape.centroid))
+                .collect(toList());
+
+        double minDistance = distanceVectors.stream()
+                .mapToDouble(Vector::mag)
+                .min().orElse(1.0);
+
+        Vector shiftBy = new Vector(0, 0, 0);
+        boolean xZero = distanceVectors.stream().allMatch(v -> Math.abs(v.x) < 1e-15);
+        if (xZero) {
+            shiftBy = shiftBy.add(new Vector(minDistance, 0, 0));
+        }
+        boolean yZero = distanceVectors.stream().allMatch(v -> Math.abs(v.y) < 1e-15);
+        if (yZero) {
+            shiftBy = shiftBy.add(new Vector(0, minDistance, 0));
+        }
+        boolean zZero = distanceVectors.stream().allMatch(v -> Math.abs(v.z) < 1e-15);
+        if (zZero) {
+            shiftBy = shiftBy.add(new Vector(0, 0, minDistance));
+        }
+
+        double[][] A = new double[neighs.length][3];
+        for (int i = 0; i < A.length; i++) {
+            Vector distance = distanceVectors.get(i).add(shiftBy);
+            A[i][0] = distance.x;
+            A[i][1] = distance.y;
+            A[i][2] = distance.z;
+        }
+
+        double[][] AT = transpose(A);
+        matrices[cell.index] = multiply(invert(multiply(AT, A)), AT);
+
+        for (int var = 0; var < cell.U.length; var++) {
+            cell.reconstructCoeffs[var] = new double[3];
+        }
+    }
+
+    @Override
+    public void reconstruct() {
+        mesh.cellStream().forEach(this::reconstructCell);
+    }
+
+    private void reconstructCell(Cell cell) {
+        for (int var = 0; var < cell.U.length; var++) {
+            reconstructVar(cell, var);
+        }
+    }
+
+    private void reconstructVar(Cell cell, int var) {
+        double[] deltaU = Arrays.stream(neighbors[cell.index])
+                .mapToDouble(neighCell -> neighCell.U[var] - cell.U[var])
+                .toArray();
+        double[] derivatives = multiply(matrices[cell.index], deltaU);
+        Vector gradient = new Vector(derivatives[0], derivatives[1], derivatives[2]);
+
+        double uMax = Arrays.stream(neighbors[cell.index])
+                .mapToDouble(neighCell -> neighCell.U[var])
+                .max().orElse(Double.POSITIVE_INFINITY);
+        uMax = Math.max(uMax, cell.U[var]);
+        double duMax = uMax - cell.U[var];
+
+        double uMin = Arrays.stream(neighbors[cell.index])
+                .mapToDouble(neighCell -> neighCell.U[var])
+                .min().orElse(Double.NEGATIVE_INFINITY);
+        uMin = Math.min(uMin, cell.U[var]);
+        double duMin = uMin - cell.U[var];
+
+        double phi_i = Arrays.stream(cell.nodes)
+                .mapToDouble(node -> reconstructValueAt(cell, var, gradient, node.location()))
+                .map(uj -> Phi(duMin, duMax, cell.U[var], uj))
+                .min().orElse(1.0);
+
+        derivatives = multiply(derivatives, phi_i);
+
+        copy(derivatives, cell.reconstructCoeffs[var]);
+    }
+
+    private double reconstructValueAt(Cell cell, int var, Vector gradient, Point at) {
+        Vector distanceVector = new Vector(cell.shape.centroid, at);
+        return cell.U[var] + gradient.dot(distanceVector);
+    }
+
+    private double Phi(double duMin, double duMax, double ui, double uj) {
+        double deltaMinus = (uj - ui);
+        if (uj - ui > 0) {
+            return phi(duMax, deltaMinus);
+        } else if (uj - ui < 0) {
+            return phi(duMin, deltaMinus);
+        } else {
+            return 1;
+        }
+    }
+
+    private double phi(double dp, double dm) {
+        double y = dp / dm;
+        return (y * y + 2.0 * y) / (y * y + y + 2.0);
+    }
+
+    @Override
+    public double[] conservativeVars(Cell cell, Point atPoint) {
+        int numVars = cell.U.length;
+        Vector r = new Vector(cell.shape.centroid, atPoint);
+        double[] dU = new double[numVars];
+        for (int var = 0; var < numVars; var++) {
+            double du_dx = cell.reconstructCoeffs[var][0];
+            double du_dy = cell.reconstructCoeffs[var][1];
+            double du_dz = cell.reconstructCoeffs[var][2];
+            Vector gradient = new Vector(du_dx, du_dy, du_dz);
+
+            dU[var] = gradient.dot(r);
+        }
+
+        return add(cell.U, dU);
+    }
+}
---
+++ test/main/solver/reconstructor/VKLimiterReconstructorTest.java
@@ -0,0 +1,150 @@
+package main.solver.reconstructor;
+
+import main.geom.Point;
+import main.geom.Vector;
+import main.geom.factory.Line;
+import main.mesh.Cell;
+import main.mesh.Mesh;
+import main.mesh.factory.Unstructured2DMesh;
+import main.physics.goveqn.factory.EulerEquations;
+import main.util.DoubleArray;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static main.util.DoubleArray.copy;
+import static main.util.DoubleArray.newFilledArray;
+import static org.junit.Assert.assertArrayEquals;
+
+public class VKLimiterReconstructorTest {
+
+    @Test
+    public void reconstruct_evaluation0() throws FileNotFoundException {
+        EulerEquations govEqn = new EulerEquations(1.4, 287);
+        Mesh mesh = new Unstructured2DMesh(
+                new File("test/test_data/mesh_unstructured_2d.cfdu"), govEqn.numVars(), Map.of());
+
+        double[] U = {1.2, 45, 75, 18, 6546135};
+        mesh.cellStream().forEach(cell -> copy(U, cell.U));
+
+        SolutionReconstructor reconstructor = new VKLimiterReconstructor(mesh);
+        reconstructor.reconstruct();
+
+        Point p1 = new Line(mesh.nodes().get(2).location(), mesh.nodes().get(4).location())
+                .centroid();
+        Point p2 = new Line(mesh.nodes().get(3).location(), mesh.nodes().get(4).location())
+                .centroid();
+        Point p3 = new Line(mesh.nodes().get(2).location(), mesh.nodes().get(3).location())
+                .centroid();
+        Cell cell = mesh.cells().get(1);
+        double[] consVars1 = reconstructor.conservativeVars(cell, p1);
+        assertArrayEquals(U, consVars1, 1e-12);
+
+        double[] consVars2 = reconstructor.conservativeVars(cell, p2);
+        assertArrayEquals(U, consVars2, 1e-12);
+
+        double[] consVars3 = reconstructor.conservativeVars(cell, p3);
+        assertArrayEquals(U, consVars3, 1e-12);
+    }
+
+    @Test
+    public void reconstruct_evaluation_smooth() throws FileNotFoundException {
+        EulerEquations govEqn = new EulerEquations(1.4, 287);
+        Mesh mesh = new Unstructured2DMesh(
+                new File("test/test_data/mesh_unstructured_2d.cfdu"), govEqn.numVars(), Map.of());
+
+        Cell cell_i = mesh.cells().get(1);
+        int numVars = cell_i.U.length;
+        Vector[] gradientsNotLimited = gradientsNotLimited();
+        mesh.cellStream().forEach(c -> copy(linearReconstruct(gradientsNotLimited, cell_i.shape.centroid, c.shape.centroid), c.U));
+
+        double[] negInf = newFilledArray(numVars, Double.NEGATIVE_INFINITY);
+        double[] posInf = newFilledArray(numVars, Double.POSITIVE_INFINITY);
+
+        Cell[] neighs = {mesh.cells().get(0), mesh.cells().get(2), mesh.cells().get(5)};
+        double[] minU = DoubleArray.min(Arrays.stream(neighs)
+                .map(cell -> cell.U)
+                .reduce(posInf, DoubleArray::min), cell_i.U);
+
+        double[] maxU = DoubleArray.max(Arrays.stream(neighs)
+                .map(cell -> cell.U)
+                .reduce(negInf, DoubleArray::max), cell_i.U);
+
+        double[] Phi = Arrays.stream(cell_i.nodes)
+                .map(n -> linearReconstruct(gradientsNotLimited, cell_i.shape.centroid, n.location()))
+                .map(Uj -> Phi(minU, maxU, cell_i.U, Uj))
+                .reduce(posInf, DoubleArray::min);
+
+        Vector[] gradientsLimited = IntStream.range(0, numVars)
+                .mapToObj(var -> gradientsNotLimited[var].mult(Phi[var]))
+                .toArray(Vector[]::new);
+
+        SolutionReconstructor reconstructor = new VKLimiterReconstructor(mesh);
+        reconstructor.reconstruct();
+
+        Point p1 = new Line(mesh.nodes().get(2).location(), mesh.nodes().get(4).location())
+                .centroid();
+        Point p2 = new Line(mesh.nodes().get(3).location(), mesh.nodes().get(4).location())
+                .centroid();
+        Point p3 = new Line(mesh.nodes().get(2).location(), mesh.nodes().get(3).location())
+                .centroid();
+        double[] consVars1 = reconstructor.conservativeVars(cell_i, p1);
+        double[] expVars1 = linearReconstruct(gradientsLimited, cell_i.shape.centroid, p1);
+        assertArrayEquals(expVars1, consVars1, 1e-8);
+
+        double[] consVars2 = reconstructor.conservativeVars(cell_i, p2);
+        double[] expVars2 = linearReconstruct(gradientsLimited, cell_i.shape.centroid, p2);
+        assertArrayEquals(expVars2, consVars2, 1e-8);
+
+        double[] consVars3 = reconstructor.conservativeVars(cell_i, p3);
+        double[] expVars3 = linearReconstruct(gradientsLimited, cell_i.shape.centroid, p3);
+        assertArrayEquals(expVars3, consVars3, 1e-8);
+    }
+
+    private static double[] Phi(double[] minU, double[] maxU, double[] Ui, double[] Uj) {
+        return IntStream.range(0, Ui.length)
+                .mapToDouble(var
+                        -> Uj[var] - Ui[var] > 0 ? phi(maxU[var], Ui[var], Uj[var])
+                        : Uj[var] - Ui[var] < 0 ? phi(minU[var], Ui[var], Uj[var])
+                        : 1.0).toArray();
+    }
+
+    private Vector[] gradientsNotLimited() {
+        Random rnd = new Random(984);
+
+        return new Vector[]{
+                new Vector(next(rnd), next(rnd), next(rnd)),
+                new Vector(next(rnd), next(rnd), next(rnd)),
+                new Vector(next(rnd), next(rnd), next(rnd)),
+                new Vector(next(rnd), next(rnd), next(rnd)),
+                new Vector(next(rnd), next(rnd), next(rnd))
+        };
+    }
+
+    private double[] linearReconstruct(Vector[] grad_i, Point pi, Point pj) {
+        Vector vr = new Vector(pi, pj);
+
+        int numVars = 5;
+        double[] Ui = {1.2, 45, 75, 18, 6546135};
+        double[] recon = new double[numVars];
+        for (int var = 0; var < numVars; var++) {
+            recon[var] = Ui[var] + grad_i[var].dot(vr);
+        }
+
+        return recon;
+    }
+
+    private static double phi(double uiMinOrMax, double ui, double uj) {
+        double y = (uiMinOrMax - ui) / (uj - ui);
+        return (y * y + 2.0 * y) / (y * y + y + 2.0);
+    }
+
+    private double next(Random rnd) {
+        return rnd.nextDouble() * rnd.nextInt(100) - 50;
+    }
+}
