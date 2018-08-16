aad77da346010a9c871ba496e353572ee673514c
Sourabh Bhat <heySourabh@gmail.com>
2018-08-16 12:55:15 +0530

    Minor refactoring

--- test/main/TestHelper.java
+++ test/main/TestHelper.java
@@ -3,7 +3,6 @@ package main;
 import main.geom.Point;
 import main.geom.Vector;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -21,15 +20,15 @@ public class TestHelper {
     }
 
     public static void assertVectorEquals(Vector expected, Vector actual, double tolerance) {
-        assertEquals("x: ", expected.x, actual.x, tolerance);
-        assertEquals("y: ", expected.y, actual.y, tolerance);
-        assertEquals("z: ", expected.z, actual.z, tolerance);
+        assertEquals("x-component of vector", expected.x, actual.x, tolerance);
+        assertEquals("y-component of vector", expected.y, actual.y, tolerance);
+        assertEquals("z-component of vector", expected.z, actual.z, tolerance);
     }
 
     public static void assertPointEquals(Point expected, Point actual, double tolerance) {
-        assertEquals("x: ", expected.x, actual.x, tolerance);
-        assertEquals("y: ", expected.y, actual.y, tolerance);
-        assertEquals("z: ", expected.z, actual.z, tolerance);
+        assertEquals("x-component of point", expected.x, actual.x, tolerance);
+        assertEquals("y-component of point", expected.y, actual.y, tolerance);
+        assertEquals("z-component of point", expected.z, actual.z, tolerance);
     }
 
     @FunctionalInterface
