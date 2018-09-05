00d81b1b6494c0acc36e8b564c3dd2c586452423
Sourabh Bhat <heySourabh@gmail.com>
2018-09-05 14:23:42 +0530

    Checking properties of QR in test

--- test/main/util/QRDecompositionTest.java
+++ test/main/util/QRDecompositionTest.java
@@ -1,9 +1,10 @@
 package main.util;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import static main.util.DoubleMatrix.column;
 import static main.util.DoubleMatrix.multiply;
-import static main.util.DoubleMatrix.stringify;
 import static main.util.TestHelper.assertMatrixEquals;
 
 public class QRDecompositionTest {
@@ -120,6 +121,26 @@ public class QRDecompositionTest {
     }
 
     @Test
+    public void checking_properties_of_qr_decomposition_of_tall_matrix() {
+        double[][] A = {
+                {1, -1, 6},
+                {1, 2, 0},
+                {1, 3, -1},
+                {1, 4, -1}
+        };
+
+        QRDecomposition qrDecomposition = new QRDecomposition(A);
+        double[][] Q = qrDecomposition.Q();
+        double[][] R = qrDecomposition.R();
+
+        assertMatrixEquals(A, multiply(Q, R), 1e-12);
+
+        assertColumnsOrthonormal(Q, 1e-12);
+
+        assertUpperTriangular(R, 1e-12);
+    }
+
+    @Test
     public void qr_decomposition_of_square_matrix_example1() {
         double[][] A = {
                 {4, 9, -14},
@@ -177,5 +198,56 @@ public class QRDecompositionTest {
         assertMatrixEquals(Q, qrDecomposition.Q(), 1e-12);
 
         assertMatrixEquals(R, qrDecomposition.R(), 1e-12);
+    }
+
+    @Test
+    public void checking_properties_of_qr_decomposition_of_square_matrix() {
+        double[][] A = {
+                {1, 2, 6},
+                {0, 0, 7},
+                {0, 5, 12}
+        };
+
+        QRDecomposition qrDecomposition = new QRDecomposition(A);
+        double[][] Q = qrDecomposition.Q();
+        double[][] R = qrDecomposition.R();
+
+        assertMatrixEquals(A, multiply(Q, R), 1e-12);
+
+        assertColumnsOrthonormal(Q, 1e-15);
+
+        assertUpperTriangular(R, 1e-15);
+    }
+
+    private void assertOrthonormal(double[] v1, double[] v2, double tolerance) {
+        // assert normalized
+        Assert.assertEquals(1, DoubleArray.norm2Sqr(v1), tolerance);
+        Assert.assertEquals(1, DoubleArray.norm2Sqr(v2), tolerance);
+
+        // assert orthogonal
+        Assert.assertEquals(0, DoubleArray.dot(v1, v2), tolerance);
+    }
+
+    private void assertColumnsOrthonormal(double[][] matrix, double tolerance) {
+        int numCols = matrix[0].length;
+
+        for (int c = 0; c < numCols; c++) {
+            double[] col1 = column(matrix, c);
+            for (int i = c + 1; i < numCols; i++) {
+                double[] col2 = column(matrix, i);
+                assertOrthonormal(col1, col2, tolerance);
+            }
+        }
+    }
+
+    private void assertUpperTriangular(double[][] matrix, double tolerance) {
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        for (int c = 0; c < numCols; c++) {
+            for (int r = c + 1; r < numRows; r++) {
+                Assert.assertEquals(0, matrix[r][c], tolerance);
+            }
+        }
     }
 }
