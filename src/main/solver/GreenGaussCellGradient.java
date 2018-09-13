e192eabf4ddb6ae54814d2441a0560fec61b1356
Sourabh Bhat <heySourabh@gmail.com>
2018-09-13 12:37:58 +0530

    Refactoring to simplify the GreenGaussCellGradient class

--- src/main/solver/GreenGaussCellGradient.java
+++ src/main/solver/GreenGaussCellGradient.java
@@ -4,55 +4,31 @@ import main.geom.Vector;
 import main.mesh.Cell;
 import main.mesh.Face;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class GreenGaussCellGradient implements CellGradientCalculator {
     @Override
     public Vector[] forCell(Cell cell) {
-        // Assuming that face average U is calculated
-        Vector[] zeroVectors = IntStream.range(0, cell.U.length)
-                .mapToObj(var -> new Vector(0, 0, 0))
-                .toArray(Vector[]::new);
+        // Assuming that face average U is calculated (face.U)
+        int numVars = cell.U.length;
 
-        Vector[] outNormalSum = cell.faces.stream()
-                .filter(face -> face.left == cell)
-                .map(this::scaledGradients)
-                .reduce(zeroVectors, this::addVectorArrays);
+        Vector[] gradients = new Vector[numVars];
+        for (int var = 0; var < numVars; var++) {
+            gradients[var] = new Vector(0, 0, 0);
+        }
 
-        Vector[] inNormalSum = cell.faces.stream()
-                .filter(face -> face.right == cell)
-                .map(this::scaledGradients)
-                .reduce(zeroVectors, this::addVectorArrays);
+        for (Face face : cell.faces) {
+            double area = face.surface.area;
+            Vector unitNormal = face.surface.unitNormal.mult(face.left == cell ? 1 : -1);
+            Vector projectedArea = unitNormal.mult(area);
+            for (int var = 0; var < numVars; var++) {
+                gradients[var] = gradients[var].add(projectedArea.mult(face.U[var]));
+            }
+        }
 
-        return multiplyToVectors(
-                subtractVectorArrays(outNormalSum, inNormalSum),
-                1.0 / cell.shape.volume);
-    }
+        double volume = cell.shape.volume;
+        for (int var = 0; var < numVars; var++) {
+            gradients[var] = gradients[var].mult(1.0 / volume);
+        }
 
-    private Vector[] scaledGradients(Face face) {
-        Vector areaProjection = face.surface.unitNormal.mult(face.surface.area);
-
-        return Arrays.stream(face.U)
-                .mapToObj(areaProjection::mult)
-                .toArray(Vector[]::new);
-    }
-
-    private Vector[] multiplyToVectors(Vector[] vectors, double scalar) {
-        return Arrays.stream(vectors)
-                .map(v -> v.mult(scalar))
-                .toArray(Vector[]::new);
-    }
-
-    private Vector[] addVectorArrays(Vector[] v1, Vector[] v2) {
-        return IntStream.range(0, v1.length)
-                .mapToObj(i -> v1[i].add(v2[i]))
-                .toArray(Vector[]::new);
-    }
-
-    private Vector[] subtractVectorArrays(Vector[] v1, Vector[] v2) {
-        return IntStream.range(0, v1.length)
-                .mapToObj(i -> v1[i].sub(v2[i]))
-                .toArray(Vector[]::new);
+        return gradients;
     }
 }
