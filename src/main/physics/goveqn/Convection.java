93d28be4b59838575d235d0b091039852e65da00
Sourabh Bhat <heySourabh@gmail.com>
2018-10-05 08:26:15 +0530

    Added some documentation

--- src/main/physics/goveqn/Convection.java
+++ src/main/physics/goveqn/Convection.java
@@ -3,9 +3,32 @@ package main.physics.goveqn;
 import main.geom.Vector;
 
 public interface Convection {
+    /**
+     * Flux required in finite volume method: Fc nx + Gc ny + Hc nz.
+     *
+     * @param conservativeVars U
+     * @param unitNormal       n
+     * @return Fc nx + Gc ny + Hc nz
+     */
     double[] flux(double[] conservativeVars, Vector unitNormal);
 
+    /**
+     * Sorted eigenvalues of Fc nx + Gc ny + Hc nz.
+     * These eigenvalues may or may not be equal to ev(Fc) nx + ev(Gc) ny + ev(Hc) nz.
+     *
+     * @param conservativeVars U
+     * @param unitNormal       n
+     * @return Sorted eigenvalues of Fc nx + Gc ny + Hc nz.
+     */
     double[] sortedEigenvalues(double[] conservativeVars, Vector unitNormal);
 
+    /**
+     * This value is same as max(|sortedEigenvalues|).
+     * However, it will be efficient to calculate only one eigenvalue using this method.
+     *
+     * @param conservativeVars U
+     * @param unitNormal       n
+     * @return maximum of absolute eigenvalues
+     */
     double maxAbsEigenvalues(double[] conservativeVars, Vector unitNormal);
 }
