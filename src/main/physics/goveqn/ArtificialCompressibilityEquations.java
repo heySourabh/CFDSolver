6678165d9720d3de90a757ff8230dc39573c83c2
Sourabh Bhat <heySourabh@gmail.com>
2018-10-05 17:30:06 +0530

    Artificial compressibility governing equations and tests

---
+++ src/main/physics/goveqn/ArtificialCompressibilityEquations.java
@@ -0,0 +1,222 @@
+package main.physics.goveqn;
+
+import main.geom.Vector;
+
+public class ArtificialCompressibilityEquations implements GoverningEquations {
+    private final double BETA;
+    private final double RHO;
+    private final double NU;
+    private final Vector GRAVITY;
+
+    public ArtificialCompressibilityEquations(double density, double dynamicViscosity, Vector gravity) {
+        this.RHO = density;
+        GRAVITY = gravity;
+        this.NU = dynamicViscosity / RHO;
+        this.BETA = 3.0;
+    }
+
+    @Override
+    public String description() {
+        return "Artificial compressibility equations for simulating " +
+                "flows with uniform density and viscosity.";
+    }
+
+    @Override
+    public int numVars() {
+        return 4;
+    }
+
+    @Override
+    public String[] conservativeVarNames() {
+        return new String[]{
+                "p/beta", "u", "v", "w"
+        };
+    }
+
+    @Override
+    public String[] primitiveVarNames() {
+        return new String[]{
+                "p", "u", "v", "w"
+        };
+    }
+
+    @Override
+    public double[] primitiveVars(double[] conservativeVars) {
+        double p_beta = conservativeVars[0];
+        double u = conservativeVars[1];
+        double v = conservativeVars[2];
+        double w = conservativeVars[3];
+
+        double p = p_beta * BETA;
+
+        return new double[]{
+                p, u, v, w
+        };
+    }
+
+    @Override
+    public double[] conservativeVars(double[] primitiveVars) {
+        double p = primitiveVars[0];
+        double u = primitiveVars[1];
+        double v = primitiveVars[2];
+        double w = primitiveVars[3];
+
+        double p_beta = p / BETA;
+
+        return new double[]{
+                p_beta, u, v, w
+        };
+    }
+
+    private final Convection convection = new Convection() {
+        @Override
+        public double[] flux(double[] conservativeVars, Vector unitNormal) {
+            double[] primitiveVars = primitiveVars(conservativeVars);
+            double p = primitiveVars[0];
+            double u = primitiveVars[1];
+            double v = primitiveVars[2];
+            double w = primitiveVars[3];
+
+            double p_rho = p / RHO;
+            double uSqr = u * u;
+            double vSqr = v * v;
+            double wSqr = w * w;
+            double uv = u * v;
+            double uw = u * w;
+            double vw = v * w;
+
+            double nx = unitNormal.x;
+            double ny = unitNormal.y;
+            double nz = unitNormal.z;
+
+            return new double[]{
+                    u * nx + v * ny + w * nz,
+                    (uSqr + p_rho) * nx + uv * ny + uw * nz,
+                    uv * nx + (vSqr + p_rho) * ny + vw * nz,
+                    uw * nx + vw * ny + (wSqr + p_rho) * nz
+            };
+        }
+
+        @Override
+        public double[] sortedEigenvalues(double[] conservativeVars, Vector unitNormal) {
+            double[] primitiveVars = primitiveVars(conservativeVars);
+            //double p = primitiveVars[0];
+            double u = primitiveVars[1];
+            double v = primitiveVars[2];
+            double w = primitiveVars[3];
+
+            double beta_rho = BETA / RHO;
+
+            double nx = unitNormal.x;
+            double ny = unitNormal.y;
+            double nz = unitNormal.z;
+
+            double Vp = u * nx + v * ny + w * nz;
+
+            double a = Math.sqrt(Vp * Vp + beta_rho);
+
+            return new double[]{
+                    Vp - a, Vp, Vp, Vp + a
+            };
+        }
+
+        @Override
+        public double maxAbsEigenvalues(double[] conservativeVars, Vector unitNormal) {
+            double[] primitiveVars = primitiveVars(conservativeVars);
+            //double p = primitiveVars[0];
+            double u = primitiveVars[1];
+            double v = primitiveVars[2];
+            double w = primitiveVars[3];
+
+            double beta_rho = BETA / RHO;
+
+            double nx = unitNormal.x;
+            double ny = unitNormal.y;
+            double nz = unitNormal.z;
+
+            double Vp = u * nx + v * ny + w * nz;
+
+            double a = Math.sqrt(Vp * Vp + beta_rho);
+
+            return Math.abs(Vp) + a;
+        }
+    };
+
+    @Override
+    public Convection convection() {
+        return convection;
+    }
+
+    private final Diffusion diffusion = new Diffusion() {
+
+        @Override
+        public double[] flux(double[] conservativeVars, Vector[] gradConservativeVars, Vector unitNormal) {
+            Vector grad_u = gradConservativeVars[1];
+            Vector grad_v = gradConservativeVars[2];
+            Vector grad_w = gradConservativeVars[3];
+
+            double du_dx = grad_u.x;
+            double du_dy = grad_u.y;
+            double du_dz = grad_u.z;
+
+            double dv_dx = grad_v.x;
+            double dv_dy = grad_v.y;
+            double dv_dz = grad_v.z;
+
+            double dw_dx = grad_w.x;
+            double dw_dy = grad_w.y;
+            double dw_dz = grad_w.z;
+
+            double tau_xx = 2.0 * NU * du_dx;
+            double tau_yy = 2.0 * NU * dv_dy;
+            double tau_zz = 2.0 * NU * dw_dz;
+
+            double tau_xy = NU * (du_dy + dv_dx);
+            double tau_yx = tau_xy;
+
+            double tau_xz = NU * (du_dz + dw_dx);
+            double tau_zx = tau_xz;
+
+            double tau_yz = NU * (dv_dz + dw_dy);
+            double tau_zy = tau_yz;
+
+            double nx = unitNormal.x;
+            double ny = unitNormal.y;
+            double nz = unitNormal.z;
+
+            return new double[]{
+                    0.0,
+                    tau_xx * nx + tau_yx * ny + tau_zx * nz,
+                    tau_xy * nx + tau_yy * ny + tau_zy * nz,
+                    tau_xz * nx + tau_yz * ny + tau_zz * nz
+            };
+        }
+
+        @Override
+        public double maxAbsDiffusivity(double[] conservativeVars) {
+            return NU;
+        }
+    };
+
+    @Override
+    public Diffusion diffusion() {
+        return diffusion;
+    }
+
+    private final Source source = new Source() {
+        @Override
+        public double[] sourceVector(double[] conservativeVars, Vector[] gradConservativeVars) {
+            return new double[]{
+                    0,
+                    GRAVITY.x,
+                    GRAVITY.y,
+                    GRAVITY.z
+            };
+        }
+    };
+
+    @Override
+    public Source source() {
+        return source;
+    }
+}
---
+++ test/main/physics/goveqn/ArtificialCompressibilityEquationsTest.java
@@ -0,0 +1,209 @@
+package main.physics.goveqn;
+
+import main.geom.Vector;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArtificialCompressibilityEquationsTest {
+
+    @Test
+    public void description() {
+        GoverningEquations govEqn = new ArtificialCompressibilityEquations(5.0,
+                1e-5, new Vector(1, -8, 7));
+        assertEquals("Artificial compressibility equations for simulating flows with uniform density and viscosity.", govEqn.description());
+    }
+
+    @Test
+    public void numVars() {
+        GoverningEquations govEqn = new ArtificialCompressibilityEquations(5.0,
+                1e-5, new Vector(1, -8, 7));
+        assertEquals(4, govEqn.numVars());
+    }
+
+    @Test
+    public void conservativeVarNames() {
+        GoverningEquations govEqn = new ArtificialCompressibilityEquations(5.0,
+                1e-5, new Vector(1, -8, 7));
+        assertArrayEquals(
+                new String[]{
+                        "p/beta", "u", "v", "w"
+                },
+                govEqn.conservativeVarNames()
+        );
+    }
+
+    @Test
+    public void primitiveVarNames() {
+        GoverningEquations govEqn = new ArtificialCompressibilityEquations(5.0,
+                1e-5, new Vector(1, -8, 7));
+        assertArrayEquals(
+                new String[]{
+                        "p", "u", "v", "w"
+                },
+                govEqn.primitiveVarNames()
+        );
+    }
+
+    @Test
+    public void primitiveVars() {
+        double rho = 5.0;
+        double mu = 1e-5;
+        Vector gravity = new Vector(1, -8, 7);
+        GoverningEquations govEqn = new ArtificialCompressibilityEquations(rho, mu, gravity);
+        double p = 120;
+        double u = 0;
+        double v = 1.2;
+        double w = -8.9;
+
+        double[] conservativeVars = {p / 3.0, u, v, w};
+
+        assertArrayEquals(new double[]{p, u, v, w}, govEqn.primitiveVars(conservativeVars), 1e-15);
+    }
+
+    @Test
+    public void conservativeVars() {
+        double rho = 5.0;
+        double mu = 1e-5;
+        Vector gravity = new Vector(1, -8, 7);
+        GoverningEquations govEqn = new ArtificialCompressibilityEquations(rho, mu, gravity);
+        double p = 120;
+        double u = 0;
+        double v = 1.2;
+        double w = -8.9;
+
+        double[] conservativeVars = {p / 3.0, u, v, w};
+
+        assertArrayEquals(conservativeVars, govEqn.conservativeVars(new double[]{p, u, v, w}), 1e-15);
+    }
+
+    @Test
+    public void convection() {
+        double rho = 5.0;
+        double mu = 1e-5;
+        Vector gravity = new Vector(1, -8, 7);
+        GoverningEquations govEqn = new ArtificialCompressibilityEquations(rho, mu, gravity);
+        double p = 120;
+        double u = 0;
+        double v = 1.2;
+        double w = -8.9;
+
+        double[] conservativeVars = {p / 3.0, u, v, w};
+
+        Vector dir = new Vector(-5, -2, 7).unit();
+
+        double[] F = new double[]{
+                u, u * u + p / rho, u * v, u * w
+        };
+
+        double[] G = new double[]{
+                v, u * v, v * v + p / rho, v * w
+        };
+        double[] H = new double[]{
+                w, u * w, v * w, w * w + p / rho
+        };
+
+        double[] expectedFlux = {
+                F[0] * dir.x + G[0] * dir.y + H[0] * dir.z,
+                F[1] * dir.x + G[1] * dir.y + H[1] * dir.z,
+                F[2] * dir.x + G[2] * dir.y + H[2] * dir.z,
+                F[3] * dir.x + G[3] * dir.y + H[3] * dir.z
+        };
+
+        assertArrayEquals(expectedFlux, govEqn.convection().flux(conservativeVars, dir), 1e-15);
+
+        double vel = u * dir.x + v * dir.y + w * dir.z;
+        double a = Math.sqrt(vel * vel + 3.0 / rho);
+        assertArrayEquals(new double[]{vel - a, vel, vel, vel + a},
+                govEqn.convection().sortedEigenvalues(conservativeVars, dir), 1e-15);
+
+        assertEquals(Math.abs(vel) + a,
+                govEqn.convection().maxAbsEigenvalues(conservativeVars, dir), 1e-15);
+    }
+
+    @Test
+    public void diffusion() {
+        double rho = 5.0;
+        double mu = 1e-5;
+        double nu = mu / rho;
+        Vector gravity = new Vector(1, -8, 7);
+        GoverningEquations govEqn = new ArtificialCompressibilityEquations(rho, mu, gravity);
+        double p = 120;
+        double u = 0;
+        double v = 1.2;
+        double w = -8.9;
+
+        double[] conservativeVars = {p / 3.0, u, v, w};
+        Vector[] gradConservativeVars = {
+                new Vector(45, 5, 54),
+                new Vector(23, 9, 3),
+                new Vector(-9, 0, 4),
+                new Vector(1.2, -68, 8)
+        };
+
+        Vector dir = new Vector(-5, -2, 7).unit();
+
+        double du_dx = gradConservativeVars[1].x;
+        double du_dy = gradConservativeVars[1].y;
+        double du_dz = gradConservativeVars[1].z;
+        double dv_dx = gradConservativeVars[2].x;
+        double dv_dy = gradConservativeVars[2].y;
+        double dv_dz = gradConservativeVars[2].z;
+        double dw_dx = gradConservativeVars[3].x;
+        double dw_dy = gradConservativeVars[3].y;
+        double dw_dz = gradConservativeVars[3].z;
+
+        double tau_xx = nu * 2.0 * du_dx;
+        double tau_xy = nu * (du_dy + dv_dx);
+        double tau_xz = nu * (du_dz + dw_dx);
+        double tau_yy = nu * 2.0 * (dv_dy);
+        double tau_yz = nu * (dv_dz + dw_dy);
+        double tau_zz = nu * 2.0 * dw_dz;
+
+        double[] F = new double[]{
+                0, tau_xx, tau_xy, tau_xz
+        };
+
+        double[] G = new double[]{
+                0, tau_xy, tau_yy, tau_yz
+        };
+        double[] H = new double[]{
+                0, tau_xz, tau_yz, tau_zz
+        };
+
+        double[] expectedFlux = {
+                F[0] * dir.x + G[0] * dir.y + H[0] * dir.z,
+                F[1] * dir.x + G[1] * dir.y + H[1] * dir.z,
+                F[2] * dir.x + G[2] * dir.y + H[2] * dir.z,
+                F[3] * dir.x + G[3] * dir.y + H[3] * dir.z
+        };
+
+        assertArrayEquals(expectedFlux, govEqn.diffusion().flux(conservativeVars, gradConservativeVars, dir), 1e-15);
+
+        assertEquals(nu, govEqn.diffusion().maxAbsDiffusivity(conservativeVars), 1e-15);
+    }
+
+    @Test
+    public void source() {
+        double rho = 5.0;
+        double mu = 1e-5;
+        double nu = mu / rho;
+        Vector gravity = new Vector(1, -8, 7);
+        GoverningEquations govEqn = new ArtificialCompressibilityEquations(rho, mu, gravity);
+        double p = 120;
+        double u = 0;
+        double v = 1.2;
+        double w = -8.9;
+
+        double[] conservativeVars = {p / 3.0, u, v, w};
+        Vector[] gradConservativeVars = {
+                new Vector(45, 5, 54),
+                new Vector(23, 9, 3),
+                new Vector(-9, 0, 4),
+                new Vector(1.2, -68, 8)
+        };
+
+        assertArrayEquals(new double[]{0, gravity.x, gravity.y, gravity.z},
+                govEqn.source().sourceVector(conservativeVars, gradConservativeVars), 1e-15);
+    }
+}
