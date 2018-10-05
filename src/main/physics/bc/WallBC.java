cce93a345f6e392c39bea726ed28afb99f54e5b1
Sourabh Bhat <heySourabh@gmail.com>
2018-10-06 00:38:07 +0530

    No-slip moving Wall bc and tests - artificial compressibility gov eqn

---
+++ src/main/physics/bc/WallBC.java
@@ -0,0 +1,50 @@
+package main.physics.bc;
+
+import main.geom.Vector;
+import main.mesh.Face;
+import main.physics.goveqn.ArtificialCompressibilityEquations;
+import main.util.DoubleArray;
+
+public class WallBC implements BoundaryCondition {
+
+    private final ArtificialCompressibilityEquations govEqn;
+    private final Vector wallVelocity;
+
+    public WallBC(ArtificialCompressibilityEquations govEqn, Vector wallVelocity) {
+        this.govEqn = govEqn;
+        this.wallVelocity = wallVelocity;
+    }
+
+    @Override
+    public void setGhostCellValues(Face face) {
+        // velocity is linearly extrapolated, pressure is simply copied
+        double[] insideConservativeVars = face.left.U;
+        double ui = insideConservativeVars[1];
+        double vi = insideConservativeVars[2];
+        double wi = insideConservativeVars[3];
+        Vector insideCellVelocity = new Vector(ui, vi, wi);
+        Vector ghostCellVelocity = this.wallVelocity.mult(2.0).sub(insideCellVelocity);
+
+        double[] ghostConservativeVars = {
+                insideConservativeVars[0],
+                ghostCellVelocity.x,
+                ghostCellVelocity.y,
+                ghostCellVelocity.z
+        };
+
+        DoubleArray.copy(ghostConservativeVars, face.right.U);
+    }
+
+    @Override
+    public double[] convectiveFlux(Face face) {
+        // u, v, w defined by the velocity of the wall, p defined by the inside cell
+        double p_rho = face.left.U[0];
+        double u = this.wallVelocity.x;
+        double v = this.wallVelocity.y;
+        double w = this.wallVelocity.z;
+
+        double[] conservativeVars = {p_rho, u, v, w};
+
+        return govEqn.convection().flux(conservativeVars, face.surface.unitNormal);
+    }
+}
---
+++ test/main/physics/bc/WallBCTest.java
@@ -0,0 +1,102 @@
+package main.physics.bc;
+
+import main.geom.Vector;
+import main.mesh.Cell;
+import main.mesh.Face;
+import main.mesh.Surface;
+import main.physics.goveqn.ArtificialCompressibilityEquations;
+import main.util.DoubleArray;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class WallBCTest {
+
+    @Test
+    public void setGhostCellValues_stationary_wall() {
+        ArtificialCompressibilityEquations govEqn =
+                new ArtificialCompressibilityEquations(2.6, 1e-4, new Vector(-1, -8, 7));
+        Cell insideCell = new Cell(null, null, null, govEqn.numVars());
+        Cell ghostCell = new Cell(null, null, null, govEqn.numVars());
+        Vector faceNormal = new Vector(1, 5, 5).unit();
+        Face face = new Face(null, null, new Surface(1.5, null, faceNormal),
+                insideCell, ghostCell, govEqn.numVars());
+
+        double p = 4654;
+        double u = 15;
+        double v = 45;
+        double w = 87;
+
+        double beta = 3.0;
+
+        DoubleArray.copy(new double[]{p / beta, u, v, w}, insideCell.U);
+
+        double[] expectedU = {p / beta, -u, -v, -w};
+
+        WallBC bc = new WallBC(govEqn, new Vector(0, 0, 0));
+        bc.setGhostCellValues(face);
+
+        assertArrayEquals(expectedU, ghostCell.U, 1e-15);
+    }
+
+    @Test
+    public void setGhostCellValues_moving_wall() {
+        ArtificialCompressibilityEquations govEqn =
+                new ArtificialCompressibilityEquations(2.6, 1e-4, new Vector(-1, -8, 7));
+        Cell insideCell = new Cell(null, null, null, govEqn.numVars());
+        Cell ghostCell = new Cell(null, null, null, govEqn.numVars());
+        Vector faceNormal = new Vector(1, 5, 5).unit();
+        Face face = new Face(null, null, new Surface(1.5, null, faceNormal),
+                insideCell, ghostCell, govEqn.numVars());
+
+        double p = 4654;
+        double u = 15;
+        double v = 45;
+        double w = 20;
+
+        double beta = 3.0;
+
+        DoubleArray.copy(new double[]{p / beta, u, v, w}, insideCell.U);
+
+        Vector wallVelocity = new Vector(-1, 4, 0);
+
+        double[] expectedU = {p / beta, -17, -37, -20};
+
+        WallBC bc = new WallBC(govEqn, wallVelocity);
+        bc.setGhostCellValues(face);
+
+        assertArrayEquals(expectedU, ghostCell.U, 1e-15);
+    }
+
+    @Test
+    public void convectiveFlux() {
+        ArtificialCompressibilityEquations govEqn =
+                new ArtificialCompressibilityEquations(2.6, 1e-4, new Vector(-1, -8, 7));
+        Cell insideCell = new Cell(null, null, null, govEqn.numVars());
+        Cell ghostCell = new Cell(null, null, null, govEqn.numVars());
+        Vector faceNormal = new Vector(1, 5, 5).unit();
+        Face face = new Face(null, null, new Surface(1.5, null, faceNormal),
+                insideCell, ghostCell, govEqn.numVars());
+
+        double p = 4654;
+        double u = 15;
+        double v = 45;
+        double w = 20;
+
+        double beta = 3.0;
+
+        DoubleArray.copy(new double[]{p / beta, u, v, w}, insideCell.U);
+
+        Vector wallVelocity = new Vector(-1, 4, 0);
+        WallBC bc = new WallBC(govEqn, wallVelocity);
+
+        double[] expectedFlux = {
+                -1 * faceNormal.x + 4 * faceNormal.y,
+                (1 + 4654 / 2.6) * faceNormal.x + (-4) * faceNormal.y,
+                (-4) * faceNormal.x + (16 + 4654 / 2.6) * faceNormal.y,
+                4654 / 2.6 * faceNormal.z
+        };
+
+        assertArrayEquals(expectedFlux, bc.convectiveFlux(face), 1e-15);
+    }
+}
