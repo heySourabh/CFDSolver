48bbbddfe5f8cdba48d435b832edc9bde2920830
Sourabh Bhat <heySourabh@gmail.com>
2018-10-13 19:14:35 +0530

    Implemented SSPRK2 with solver test

---
+++ src/main/solver/time/ExplicitSSPRK2TimeIntegrator.java
@@ -0,0 +1,146 @@
+package main.solver.time;
+
+import main.mesh.Cell;
+import main.mesh.Mesh;
+import main.solver.Norm;
+import main.solver.SpaceDiscretization;
+import main.util.DoubleArray;
+
+import java.util.stream.Stream;
+
+import static main.util.DoubleArray.*;
+
+public class ExplicitSSPRK2TimeIntegrator implements TimeIntegrator {
+
+    private final Mesh mesh;
+    private final SpaceDiscretization spaceDiscretization;
+    private final int numVars;
+    private final double[][] U;
+    private final TimeStep timeStep;
+    private double courantNum = 2.0; // default
+    private TimeDiscretization realTimeDiscretization = null; // default
+
+    public ExplicitSSPRK2TimeIntegrator(Mesh mesh, SpaceDiscretization spaceDiscretization, TimeStep timeStep, int numVars) {
+        this.mesh = mesh;
+        this.spaceDiscretization = spaceDiscretization;
+        this.timeStep = timeStep;
+        this.numVars = numVars;
+        int numCells = mesh.cells().size();
+        this.U = new double[numCells][numVars];
+    }
+
+    @Override
+    public void setCourantNum(double courantNum) {
+        this.courantNum = courantNum;
+    }
+
+    @Override
+    public void setTimeDiscretization(TimeDiscretization timeDiscretization) {
+        this.realTimeDiscretization = timeDiscretization;
+    }
+
+    @Override
+    public void updateCellAverages() {
+        saveCurrentAverages();
+
+        // Zeroth stage
+        setResidualForAllCells();
+        setTimeStepForAllCells();
+        calculateNewAveragesStage0();
+
+        // First stage
+        setResidualForAllCells();
+        calculateNewAveragesStage1();
+    }
+
+    @Override
+    public double[] currentTotalResidual(Norm norm) {
+        double[] zeros = new double[numVars];
+
+        Stream<double[]> absResidualStream = mesh.cellStream()
+                .map(cell -> apply(cell.U, U[cell.index()], (e1, e2) -> (e1 - e2) / cell.dt))
+                .map(DoubleArray::abs);
+
+        int numCells = mesh.cells().size();
+        double[] totalResidue;
+        switch (norm) {
+            case ONE_NORM:
+                totalResidue = absResidualStream
+                        .reduce(zeros, DoubleArray::add);
+                totalResidue = multiply(totalResidue, 1.0 / numCells);
+                break;
+
+            case TWO_NORM:
+                totalResidue = absResidualStream
+                        .map(DoubleArray::sqr)
+                        .reduce(zeros, DoubleArray::add);
+                totalResidue = apply(totalResidue, Math::sqrt);
+                totalResidue = multiply(totalResidue, 1.0 / numCells);
+                break;
+
+            case INFINITY_NORM:
+                totalResidue = absResidualStream
+                        .reduce(zeros, (d1, d2) -> apply(d1, d2, Math::max));
+                break;
+
+            default:
+                throw new IllegalArgumentException("Norm " + norm + " is not implemented.");
+        }
+
+        // normalize
+        double[] varMagnitude = mesh.cellStream()
+                .map(cell -> cell.U)
+                .map(DoubleArray::abs)
+                .reduce(zeros, DoubleArray::add);
+        varMagnitude = multiply(varMagnitude, 1.0 / numCells);
+        varMagnitude = apply(varMagnitude, e -> (e < 1e-12 ? 1.0 : e)); // avoid div by zero
+        for (int i = 0; i < numVars; i++) {
+            totalResidue[i] /= varMagnitude[i];
+        }
+
+        return totalResidue;
+    }
+
+    private void saveCurrentAverages() {
+        mesh.cellStream().forEach(cell -> copy(cell.U, U[cell.index()]));
+    }
+
+    private void setResidualForAllCells() {
+        spaceDiscretization.setResiduals();
+        if (realTimeDiscretization != null)
+            realTimeDiscretization.updateCellResiduals();
+    }
+
+    private void setTimeStepForAllCells() {
+        double real_dt = realTimeDiscretization != null
+                ? realTimeDiscretization.dt()
+                : Double.POSITIVE_INFINITY;
+        timeStep.updateCellTimeSteps(courantNum, real_dt * 0.66);
+    }
+
+    private void calculateNewAveragesStage0() {
+        mesh.cellStream().forEach(this::calculateNewAveragesStage0);
+    }
+
+    private void calculateNewAveragesStage1() {
+        mesh.cellStream().forEach(this::calculateNewAveragesStage1);
+    }
+
+    private void calculateNewAveragesStage0(Cell cell) {
+        double dt_vol = cell.dt / cell.shape.volume;
+        double[] U = subtract(cell.U, multiply(cell.residual, dt_vol));
+        copy(U, cell.U);
+    }
+
+    private void calculateNewAveragesStage1(Cell cell) {
+        double dt_vol = cell.dt / cell.shape.volume;
+        int numVars = cell.U.length;
+
+        double[] U = new double[numVars];
+        for (int var = 0; var < numVars; var++) {
+            U[var] = 0.5 * (this.U[cell.index()][var] + cell.U[var] - dt_vol * cell.residual[var]);
+        }
+
+        copy(U, cell.U);
+    }
+}
---
+++ test/main/SolverTransientLidDrivenCavity2DSSPRK2Test.java
@@ -0,0 +1,197 @@
+package main;
+
+import main.geom.Vector;
+import main.io.VTKWriter;
+import main.mesh.Mesh;
+import main.mesh.factory.Structured2DMesh;
+import main.physics.bc.BoundaryCondition;
+import main.physics.bc.WallBC;
+import main.physics.goveqn.ArtificialCompressibilityEquations;
+import main.physics.goveqn.GoverningEquations;
+import main.solver.*;
+import main.solver.convection.ConvectionResidual;
+import main.solver.convection.reconstructor.VKLimiterReconstructor;
+import main.solver.convection.riemann.RusanovRiemannSolver;
+import main.solver.diffusion.DiffusionResidual;
+import main.solver.problem.ProblemDefinition;
+import main.solver.time.*;
+import main.util.DoubleArray;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class SolverTransientLidDrivenCavity2DSSPRK2Test {
+
+    private final ProblemDefinition problem = new ProblemDefinition() {
+        private final double Re = 100;
+        private final double L = 1.0;
+        private final double rho = 1.0;
+        private final double lidVelocity = 1.0;
+        private final double mu = rho * lidVelocity * L / Re;
+        private final Vector gravity = new Vector(0, 0, 0);
+
+        private final ArtificialCompressibilityEquations govEqn
+                = new ArtificialCompressibilityEquations(1.0, mu, gravity);
+
+        private final Mesh mesh = create2DMesh(20, 20);
+
+        private Mesh create2DMesh(int numXCells, int numYCells) {
+            int numXNodes = numXCells + 1;
+            int numYNodes = numYCells + 1;
+            double minX = 0, minY = 0;
+            double maxX = minX + L;
+            double maxY = minY + L;
+            File tempMeshFile = new File("test/test_data/lid_driven_cavity_mesh");
+
+            try (FileWriter fileWriter = new FileWriter(tempMeshFile);
+                 PrintWriter writer = new PrintWriter(fileWriter)) {
+                writer.write("dimension = 2\n");
+                writer.write("mode = ASCII\n");
+                writer.printf("xi = %d\n", numXNodes);
+                writer.printf("eta = %d\n", numYNodes);
+                for (int i = 0; i < numXNodes; i++) {
+                    double x = minX + i / (numXNodes - 1.0) * (maxX - minX);
+                    for (int j = 0; j < numYNodes; j++) {
+                        double y = minY + j / (numYNodes - 1.0) * (maxY - minY);
+                        writer.printf("%-20.15f %-20.15f %-20.15f\n", x, y, 0.0);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Unable to create mesh.");
+            }
+
+            BoundaryCondition stationaryWall = new WallBC(govEqn, new Vector(0, 0, 0));
+            BoundaryCondition movingLid = new WallBC(govEqn, new Vector(lidVelocity, 0, 0));
+            Mesh mesh = null;
+            try {
+                mesh = new Structured2DMesh(tempMeshFile, govEqn.numVars(), stationaryWall, stationaryWall, stationaryWall, movingLid);
+                if (!tempMeshFile.delete()) {
+                    System.out.println("Unable to delete temporary file: " + tempMeshFile);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Mesh file is not found.");
+            }
+            return mesh;
+        }
+
+        private final SolutionInitializer solutionInitializer = new FunctionInitializer(
+                p -> new double[]{0, 0, 0, 0});
+
+
+        CellNeighborCalculator cellNeighborCalculator = new FaceBasedCellNeighbors();
+        CellGradientCalculator cellGradientCalculator = new LeastSquareCellGradient(mesh, cellNeighborCalculator);
+        private final ConvectionResidual convectionResidual = new ConvectionResidual(
+                new VKLimiterReconstructor(mesh, cellGradientCalculator, cellNeighborCalculator),
+                new RusanovRiemannSolver(govEqn), mesh);
+        private final DiffusionResidual diffusionResidual = new DiffusionResidual(mesh, govEqn);
+        private final SpaceDiscretization spaceDiscretization = new SpaceDiscretization(mesh,
+                List.of(convectionResidual, diffusionResidual));
+        private final TimeStep timeStep = new LocalTimeStep(mesh, govEqn);
+
+        private final TimeIntegrator timeIntegrator =
+                new ExplicitSSPRK2TimeIntegrator(mesh, spaceDiscretization, timeStep, govEqn.numVars());
+
+        private final Convergence convergence = new Convergence(DoubleArray.newFilledArray(govEqn.numVars(), 1e-3));
+
+        private final Config config = createConfig();
+
+        private Config createConfig() {
+            Config config = new Config();
+            config.setMaxIterations(10000);
+            return config;
+        }
+
+        @Override
+        public String description() {
+            return "Lid Driven Cavity.";
+        }
+
+        @Override
+        public GoverningEquations govEqn() {
+            return govEqn;
+        }
+
+        @Override
+        public Mesh mesh() {
+            return mesh;
+        }
+
+        @Override
+        public SolutionInitializer solutionInitializer() {
+            return solutionInitializer;
+        }
+
+        @Override
+        public TimeIntegrator timeIntegrator() {
+            return timeIntegrator;
+        }
+
+        @Override
+        public Convergence convergence() {
+            return convergence;
+        }
+
+        @Override
+        public Config config() {
+            return config;
+        }
+    };
+
+    @Test
+    public void solver() throws IOException {
+        Mesh mesh = problem.mesh();
+        problem.solutionInitializer().initialize(mesh, problem.govEqn());
+
+        TimeIntegrator timeIntegrator = problem.timeIntegrator();
+        timeIntegrator.setCourantNum(2.0);
+        TimeDiscretization timeDiscretization = new TwoPointTimeDiscretization(mesh, problem.govEqn(), 0.1);
+        timeIntegrator.setTimeDiscretization(timeDiscretization);
+        Config config = problem.config();
+        Convergence convergence = problem.convergence();
+
+        int maxPseudoIter = config.getMaxIterations();
+        int numRealIter = 100;
+
+        int[] expectedPseudoIterations = {
+                300, 254, 320, 194, 172, 145, 121, 105, 92, 82, 72, 65, 58, 53, 48, 45, 41, 39, 36, 34, 32, 31,
+                29, 27, 26, 24, 23, 21, 20, 18, 18, 15, 15, 13, 12, 11, 10, 8, 9, 5, 6, 6, 4, 4, 4, 4, 3, 3, 3,
+                3, 3, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+
+        int[] actualPseudoIterations = new int[numRealIter];
+
+        File outputFolder = new File("test/test_data/transient_lid_driven_cavity_SSPRK2/");
+        if (!outputFolder.mkdirs() && !outputFolder.exists())
+            throw new IOException("Unable to create required folders for writing output.");
+        for (int real_time_iter = 0; real_time_iter < numRealIter; real_time_iter++) {
+            new VTKWriter(new File(outputFolder,
+                    String.format("sol_%05d.vtu", real_time_iter)),
+                    mesh, problem.govEqn()).write();
+            int pseudoIter = 0;
+            for (; pseudoIter < maxPseudoIter; pseudoIter++) {
+                timeIntegrator.updateCellAverages();
+                double[] residual = timeIntegrator.currentTotalResidual(config.getConvergenceNorm());
+                System.out.println(pseudoIter + ": " + Arrays.toString(residual));
+                if (convergence.hasConverged(residual)) {
+                    System.out.println("Converged.");
+                    break;
+                }
+            }
+            actualPseudoIterations[real_time_iter] = pseudoIter;
+            timeDiscretization.shiftSolution();
+            if (real_time_iter == 0) {
+                timeDiscretization = new ThreePointTimeDiscretization(mesh, problem.govEqn(), timeDiscretization.dt());
+                timeIntegrator.setTimeDiscretization(timeDiscretization);
+            }
+        }
+        new VTKWriter(new File(outputFolder,
+                String.format("sol_%05d.vtu", numRealIter)),
+                mesh, problem.govEqn()).write();
+
+        Assert.assertArrayEquals(expectedPseudoIterations, actualPseudoIterations);
+    }
+}
