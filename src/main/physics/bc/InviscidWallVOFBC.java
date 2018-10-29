ceed24ba8123664f4bbaf40ef96a0ef01d923af2
Sourabh Bhat <heySourabh@gmail.com>
2018-10-30 01:28:26 +0530

    Inviscid wall BC for VOF equations

---
+++ src/main/physics/bc/InviscidWallVOFBC.java
@@ -0,0 +1,51 @@
+package main.physics.bc;
+
+import main.geom.Vector;
+import main.mesh.Face;
+import main.physics.goveqn.factory.ArtificialCompressibilityVOFEquations;
+
+import static main.util.DoubleArray.copy;
+
+public class InviscidWallVOFBC implements BoundaryCondition {
+    private final ArtificialCompressibilityVOFEquations govEqn;
+
+    public InviscidWallVOFBC(ArtificialCompressibilityVOFEquations govEqn) {
+        this.govEqn = govEqn;
+    }
+
+    @Override
+    public void setGhostCellValues(Face face) {
+        // extrapolate pressure and C
+        // mirror velocity vector
+        double[] insidePrimVars = govEqn.primitiveVars(face.left.U);
+        double p = insidePrimVars[0];
+        double u = insidePrimVars[1];
+        double v = insidePrimVars[2];
+        double w = insidePrimVars[3];
+        double C = insidePrimVars[4];
+
+        Vector insideVelocity = new Vector(u, v, w);
+        Vector normalVelocity = face.surface.unitNormal
+                .mult(insideVelocity.dot(face.surface.unitNormal));
+        Vector tangentVelocity = insideVelocity.sub(normalVelocity);
+        Vector mirroredVelocity = normalVelocity
+                .mult(-1)
+                .add(tangentVelocity);
+        double[] ghostCellPrimVars = new double[]{
+                p, mirroredVelocity.x, mirroredVelocity.y, mirroredVelocity.z, C
+        };
+        double[] ghostCellConsVars = govEqn.conservativeVars(ghostCellPrimVars);
+
+        copy(ghostCellConsVars, face.right.U);
+    }
+
+    @Override
+    public double[] convectiveFlux(Face face) {
+        Vector n = face.surface.unitNormal;
+        double[] insidePrimVars = govEqn.primitiveVars(face.left.U);
+        double p = insidePrimVars[0];
+        return new double[]{
+                0, p * n.x, p * n.y, p * n.z, 0
+        };
+    }
+}
---
+++ test/main/physics/bc/InviscidWallVOFBCTest.java
@@ -0,0 +1,139 @@
+package main.physics.bc;
+
+import main.geom.Vector;
+import main.mesh.Cell;
+import main.mesh.Face;
+import main.mesh.Surface;
+import main.physics.goveqn.factory.ArtificialCompressibilityVOFEquations;
+import main.util.DoubleArray;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class InviscidWallVOFBCTest {
+
+    @Test
+    public void setGhostCellValues() {
+        double p = 542;
+        double u = 6545;
+        double v = 81;
+        double w = 86;
+        double C = 0.8;
+
+        double rho1 = 841;
+        double mu1 = 0.001;
+        double rho2 = 20;
+        double mu2 = 14;
+
+        double beta = 654;
+
+        Vector gravity = new Vector(54, 7, -78);
+
+        ArtificialCompressibilityVOFEquations govEqn = new ArtificialCompressibilityVOFEquations(
+                rho1, mu1, rho2, mu2, gravity, beta);
+
+        double[] conservativeVars = govEqn.conservativeVars(new double[]{p, u, v, w, C});
+
+        BoundaryCondition bc = new InviscidWallVOFBC(govEqn);
+        Cell left = new Cell(null, null, null, govEqn.numVars());
+        Cell ghost = new Cell(null, null, null, govEqn.numVars());
+        Surface surface1 = new Surface(1.0, null, new Vector(1, 0, 0));
+        Face face = new Face(null, null, surface1, left, ghost, govEqn.numVars());
+        DoubleArray.copy(conservativeVars, face.left.U);
+
+        bc.setGhostCellValues(face);
+
+        double[] expectedGhostCellValues = govEqn.conservativeVars(new double[]{
+                p, -u, v, w, C
+        });
+
+        assertArrayEquals(expectedGhostCellValues, face.right.U, 1e-15);
+
+
+        Surface surface2 = new Surface(1.0, null, new Vector(0, 1, 0));
+        face = new Face(null, null, surface2, left, ghost, govEqn.numVars());
+        DoubleArray.copy(conservativeVars, face.left.U);
+
+        bc.setGhostCellValues(face);
+
+        expectedGhostCellValues = govEqn.conservativeVars(new double[]{
+                p, u, -v, w, C
+        });
+
+        assertArrayEquals(expectedGhostCellValues, face.right.U, 1e-15);
+
+
+        Surface surface3 = new Surface(1.0, null, new Vector(0, 0, 1));
+        face = new Face(null, null, surface3, left, ghost, govEqn.numVars());
+        DoubleArray.copy(conservativeVars, face.left.U);
+
+        bc.setGhostCellValues(face);
+
+        expectedGhostCellValues = govEqn.conservativeVars(new double[]{
+                p, u, v, -w, C
+        });
+
+        assertArrayEquals(expectedGhostCellValues, face.right.U, 1e-15);
+    }
+
+    @Test
+    public void convectiveFlux() {
+        double p = 542;
+        double u = 6545;
+        double v = 81;
+        double w = 86;
+        double C = 0.8;
+
+        double rho1 = 841;
+        double mu1 = 0.001;
+        double rho2 = 20;
+        double mu2 = 14;
+
+        double beta = 654;
+
+        Vector gravity = new Vector(54, 7, -78);
+
+        ArtificialCompressibilityVOFEquations govEqn = new ArtificialCompressibilityVOFEquations(
+                rho1, mu1, rho2, mu2, gravity, beta);
+
+        double[] conservativeVars = govEqn.conservativeVars(new double[]{p, u, v, w, C});
+
+        BoundaryCondition bc = new InviscidWallVOFBC(govEqn);
+        Cell left = new Cell(null, null, null, govEqn.numVars());
+        Cell ghost = new Cell(null, null, null, govEqn.numVars());
+        Surface surface1 = new Surface(1.0, null, new Vector(1, 0, 0));
+        Face face = new Face(null, null, surface1, left, ghost, govEqn.numVars());
+        DoubleArray.copy(conservativeVars, face.left.U);
+
+        double[] expectedFlux = {
+                0, p, 0, 0, 0
+        };
+
+        double[] actualFlux = bc.convectiveFlux(face);
+        assertArrayEquals(expectedFlux, actualFlux, 1e-15);
+
+
+        Surface surface2 = new Surface(1.0, null, new Vector(0, 1, 0));
+        face = new Face(null, null, surface2, left, ghost, govEqn.numVars());
+        DoubleArray.copy(conservativeVars, face.left.U);
+
+        expectedFlux = new double[]{
+                0, 0, p, 0, 0
+        };
+
+        actualFlux = bc.convectiveFlux(face);
+        assertArrayEquals(expectedFlux, actualFlux, 1e-15);
+
+
+        Surface surface3 = new Surface(1.0, null, new Vector(0, 0, 1));
+        face = new Face(null, null, surface3, left, ghost, govEqn.numVars());
+        DoubleArray.copy(conservativeVars, face.left.U);
+
+        expectedFlux = new double[]{
+                0, 0, 0, p, 0
+        };
+
+        actualFlux = bc.convectiveFlux(face);
+        assertArrayEquals(expectedFlux, actualFlux, 1e-15);
+    }
+}
