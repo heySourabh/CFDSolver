350010a49d92ca40abfca3692dde5522059decc5
Sourabh Bhat <heySourabh@gmail.com>
2018-11-01 00:50:14 +0530

    Monir changes

--- test/main/SolverSloshing2DHLLStructuredTest.java
+++ test/main/SolverSloshing2DHLLStructuredTest.java
@@ -196,19 +196,18 @@ public class SolverSloshing2DHLLStructuredTest {
                     , time, "xi min");
             System.out.println("Time: " + time);
             int pseudoIter = 0;
-            double[] residual = null;
             for (; pseudoIter < maxPseudoIter; pseudoIter++) {
                 timeIntegrator.updateCellAverages();
-                residual = timeIntegrator.currentTotalResidual(config.getConvergenceNorm());
+                double[] residual = timeIntegrator.currentTotalResidual(config.getConvergenceNorm());
                 if (pseudoIter % 100 == 0) {
                     System.out.println(pseudoIter + ": " + Arrays.toString(residual));
                 }
                 if (convergence.hasConverged(residual)) {
+                    System.out.println(pseudoIter + ": " + Arrays.toString(residual));
                     System.out.println("Converged.");
                     break;
                 }
             }
-            System.out.println(pseudoIter + ": " + Arrays.toString(residual));
             actualPseudoIterations[real_time_iter] = pseudoIter;
             timeDiscretization.shiftSolution();
             if (real_time_iter == 0) {
