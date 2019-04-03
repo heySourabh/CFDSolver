26876754f5e23ea707898db6bdb740070e27f861
Sourabh Bhat <heySourabh@gmail.com>
2019-04-03 16:02:21 +0530

    Added HLLC-AC Riemann solver and tests

---
+++ src/main/solver/convection/riemann/HLLC_AC_RiemannSolver.java
@@ -0,0 +1,92 @@
+package main.solver.convection.riemann;
+
+import main.geom.Vector;
+import main.mesh.Surface;
+import main.physics.goveqn.factory.ArtificialCompressibilityEquations;
+
+import java.util.Arrays;
+
+public class HLLC_AC_RiemannSolver implements RiemannSolver {
+
+    private final ArtificialCompressibilityEquations govEqn;
+    private final int numVars;
+
+    public HLLC_AC_RiemannSolver(ArtificialCompressibilityEquations govEqn) {
+        this.govEqn = govEqn;
+        this.numVars = govEqn.numVars();
+    }
+
+    @Override
+    public double[] flux(double[] UL, double[] UR, Surface surface) {
+        Vector n = surface.unitNormal();
+        Vector t1 = surface.unitTangent1();
+        Vector t2 = surface.unitTangent2();
+
+        UL = rotateU(UL, n, t1, t2);
+        UR = rotateU(UR, n, t1, t2);
+
+        double[] FL = govEqn.F(UL);
+        double[] FR = govEqn.F(UR);
+
+        double[] eigenvaluesL = govEqn.min_max_eigenvalues(UL);
+        double[] eigenvaluesR = govEqn.min_max_eigenvalues(UR);
+
+        double SL = Math.min(eigenvaluesL[0], eigenvaluesR[0]);
+        double SR = Math.max(eigenvaluesL[1], eigenvaluesR[1]);
+
+        double SL_minus_SR = SL - SR;
+
+        double SStar = (FR[1] - FL[1] - SR * UR[1] + SL * UL[1]) / SL_minus_SR;
+
+        double[] flux;
+        if (SStar >= 0) {
+            double p_betaStar = (UR[1] - UL[1] - SR * UR[0] + SL * UL[0]) / SL_minus_SR;
+            double SL_minus_SStar = SL - SStar;
+            double vStarL = (SL * UL[2] - FL[2]) / SL_minus_SStar;
+            double wStarL = (SL * UL[3] - FL[3]) / SL_minus_SStar;
+            double[] UStarL = {
+                    p_betaStar, SStar, vStarL, wStarL
+            };
+            flux = new double[numVars];
+            for (int var = 0; var < numVars; var++) {
+                flux[var] = FL[var] + SL * (UStarL[var] - UL[var]);
+            }
+        } else if (SStar <= 0) {
+            double p_betaStar = (UR[1] - UL[1] - SR * UR[0] + SL * UL[0]) / SL_minus_SR;
+            double SR_minus_SStar = SR - SStar;
+            double vStarR = (SR * UR[2] - FR[2]) / SR_minus_SStar;
+            double wStarR = (SR * UR[3] - FR[3]) / SR_minus_SStar;
+            double[] UStarR = {
+                    p_betaStar, SStar, vStarR, wStarR
+            };
+            flux = new double[numVars];
+            for (int var = 0; var < numVars; var++) {
+                flux[var] = FR[var] + SR * (UStarR[var] - UR[var]);
+            }
+        } else {
+            throw new IllegalStateException("The wave speeds are not valid." +
+                    "\nRotated UL = " + Arrays.toString(UL) +
+                    "\nRotated UR = " + Arrays.toString(UR));
+        }
+
+        return cartesianF(flux, n, t1, t2);
+    }
+
+    private double[] rotateU(double[] U, Vector n, Vector t1, Vector t2) {
+        return new double[]{
+                U[0],
+                U[1] * n.x + U[2] * n.y + U[3] * n.z,
+                U[1] * t1.x + U[2] * t1.y + U[3] * t1.z,
+                U[1] * t2.x + U[2] * t2.y + U[3] * t2.z
+        };
+    }
+
+    private double[] cartesianF(double[] F, Vector n, Vector t1, Vector t2) {
+        return new double[]{
+                F[0],
+                F[1] * n.x + F[2] * t1.x + F[3] * t2.x,
+                F[1] * n.y + F[2] * t1.y + F[3] * t2.y,
+                F[1] * n.z + F[2] * t1.z + F[3] * t2.z
+        };
+    }
+}
---
+++ test/main/solver/convection/riemann/HLLC_AC_RiemannSolverTest.java
@@ -0,0 +1,208 @@
+package main.solver.convection.riemann;
+
+import main.geom.Vector;
+import main.mesh.Surface;
+import main.physics.goveqn.factory.ArtificialCompressibilityEquations;
+import main.util.DoubleArray;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static main.util.DoubleArray.*;
+import static org.junit.Assert.*;
+
+public class HLLC_AC_RiemannSolverTest {
+
+    @Test
+    public void flux_with_same_states() {
+        double[] U = {1.5, -204, -4, 5};
+        double[] UL = copyOf(U);
+        double[] UR = copyOf(U);
+
+        double rho = 61.7;
+        double mu = 545;
+        Vector gravity = new Vector(2, 6, -19);
+        double beta = 1.0;
+
+        HLLC_AC_RiemannSolver solver
+                = new HLLC_AC_RiemannSolver(
+                new ArtificialCompressibilityEquations(rho, mu, gravity));
+
+        // x-split
+        Vector unitNormal = new Vector(1, 0, 0).unit();
+        Surface surface = new Surface(2.5, null, unitNormal);
+        double[] actualFlux = solver.flux(UL, UR, surface);
+
+        double[] expectedFlux = F(U, rho, beta);
+        assertArrayEquals(expectedFlux, actualFlux, 1e-15);
+
+        // y-split
+        unitNormal = new Vector(0, 1, 0).unit();
+        surface = new Surface(2.5, null, unitNormal);
+        actualFlux = solver.flux(UL, UR, surface);
+
+        expectedFlux = G(U, rho, beta);
+        assertArrayEquals(expectedFlux, actualFlux, 1e-15);
+
+        // z-split
+        unitNormal = new Vector(0, 0, 1).unit();
+        surface = new Surface(2.5, null, unitNormal);
+        actualFlux = solver.flux(UL, UR, surface);
+
+        expectedFlux = H(U, rho, beta);
+        assertArrayEquals(expectedFlux, actualFlux, 1e-15);
+    }
+
+    @Test
+    public void flux_resulting_from_StarL_xsplit() {
+        double rho = 28.5;
+        double mu = 13.76;
+        Vector gravity = new Vector(-9.6, 56.4, 23.4);
+
+        double beta = 1.0;
+
+        HLLC_AC_RiemannSolver solver
+                = new HLLC_AC_RiemannSolver(
+                new ArtificialCompressibilityEquations(rho, mu, gravity));
+
+        double pL = 457;
+        double uL = 67;
+        double vL = -76.8;
+        double wL = 23.7;
+
+        double pR = 2.7;
+        double uR = 76;
+        double vR = 1;
+        double wR = 20.7;
+
+        double[] UL = {pL / beta, uL, vL, wL};
+        double[] UR = {pR / beta, uR, vR, wR};
+
+        double SL = Math.min(minEigenvalue(uL, beta, rho), minEigenvalue(uR, beta, rho));
+        double SR = Math.max(maxEigenvalue(uL, beta, rho), maxEigenvalue(uR, beta, rho));
+
+        double SStar = ((uR * uR + pR / rho) - (uL * uL + pL / rho) - SR * uR + SL * uL) / (SL - SR);
+
+        assertTrue(SStar > 0);
+        double p_betaStar = (uR - uL - SR * pR / beta + SL * pL / beta) / (SL - SR);
+        double vStarL = (SL * vL - uL * vL) / (SL - SStar);
+        double vStarR = (SR * vR - uR * vR) / (SR - SStar);
+        double wStarL = (SL * wL - uL * wL) / (SL - SStar);
+        double wStarR = (SR * wR - uR * wR) / (SR - SStar);
+
+        Vector unitNormal = new Vector(1, 0, 0).unit();
+        Surface surface = new Surface(2.5, null, unitNormal);
+        double[] actualFlux = solver.flux(UL, UR, surface);
+
+        double[] FL = F(UL, rho, beta);
+        double[] FR = F(UR, rho, beta);
+
+        double[] UStarL = new double[]{p_betaStar, SStar, vStarL, wStarL};
+        double[] FStarL = add(FL, multiply(subtract(UStarL, UL), SL));
+
+        double[] UStarR = new double[]{p_betaStar, SStar, vStarR, wStarR};
+        double[] FStarR = add(FR, multiply(subtract(UStarR, UR), SR));
+
+        assertArrayEquals(FStarL, actualFlux, 1e-15);
+    }
+
+    @Test
+    public void flux_resulting_from_StarR_xsplit() {
+        double rho = 28.5;
+        double mu = 13.76;
+        Vector gravity = new Vector(-9.6, 56.4, 23.4);
+
+        double beta = 1.0;
+
+        HLLC_AC_RiemannSolver solver
+                = new HLLC_AC_RiemannSolver(
+                new ArtificialCompressibilityEquations(rho, mu, gravity));
+
+        double pL = 457;
+        double uL = -67;
+        double vL = -76.8;
+        double wL = 23.7;
+
+        double pR = 2.7;
+        double uR = -76;
+        double vR = 1;
+        double wR = 20.7;
+
+        double[] UL = {pL / beta, uL, vL, wL};
+        double[] UR = {pR / beta, uR, vR, wR};
+
+        double SL = Math.min(minEigenvalue(uL, beta, rho), minEigenvalue(uR, beta, rho));
+        double SR = Math.max(maxEigenvalue(uL, beta, rho), maxEigenvalue(uR, beta, rho));
+
+        double SStar = ((uR * uR + pR / rho) - (uL * uL + pL / rho) - SR * uR + SL * uL) / (SL - SR);
+
+        assertTrue(SStar < 0);
+        double p_betaStar = (uR - uL - SR * pR / beta + SL * pL / beta) / (SL - SR);
+        double vStarL = (SL * vL - uL * vL) / (SL - SStar);
+        double vStarR = (SR * vR - uR * vR) / (SR - SStar);
+        double wStarL = (SL * wL - uL * wL) / (SL - SStar);
+        double wStarR = (SR * wR - uR * wR) / (SR - SStar);
+
+        Vector unitNormal = new Vector(1, 0, 0).unit();
+        Surface surface = new Surface(2.5, null, unitNormal);
+        double[] actualFlux = solver.flux(UL, UR, surface);
+
+        double[] FL = F(UL, rho, beta);
+        double[] FR = F(UR, rho, beta);
+
+        double[] UStarL = new double[]{p_betaStar, SStar, vStarL, wStarL};
+        double[] FStarL = add(FL, multiply(subtract(UStarL, UL), SL));
+
+        double[] UStarR = new double[]{p_betaStar, SStar, vStarR, wStarR};
+        double[] FStarR = add(FR, multiply(subtract(UStarR, UR), SR));
+
+        assertArrayEquals(FStarR, actualFlux, 1e-15);
+    }
+
+    private double[] F(double[] U, double rho, double beta) {
+        double p_beta = U[0];
+        double u = U[1];
+        double v = U[2];
+        double w = U[3];
+
+        double p = p_beta * beta;
+
+        return new double[]{
+                u, u * u + p / rho, u * v, u * w
+        };
+    }
+
+    private double[] G(double[] U, double rho, double beta) {
+        double p_beta = U[0];
+        double u = U[1];
+        double v = U[2];
+        double w = U[3];
+
+        double p = p_beta * beta;
+
+        return new double[]{
+                v, u * v, v * v + p / rho, v * w
+        };
+    }
+
+    private double[] H(double[] U, double rho, double beta) {
+        double p_beta = U[0];
+        double u = U[1];
+        double v = U[2];
+        double w = U[3];
+
+        double p = p_beta * beta;
+
+        return new double[]{
+                w, u * w, v * w, w * w + p / rho
+        };
+    }
+
+    private double minEigenvalue(double vel, double beta, double rho) {
+        return vel - Math.sqrt(vel * vel + beta / rho);
+    }
+
+    private double maxEigenvalue(double vel, double beta, double rho) {
+        return vel + Math.sqrt(vel * vel + beta / rho);
+    }
+}
