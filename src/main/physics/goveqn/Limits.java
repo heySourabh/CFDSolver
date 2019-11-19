27d40789cff05bef0e3c71177a237de8f9abcac6
Sourabh Bhat <heySourabh@gmail.com>
2019-11-19 21:50:12 +0530

    Added equals and hashcode methods.

--- src/main/physics/goveqn/Limits.java
+++ src/main/physics/goveqn/Limits.java
@@ -9,4 +9,26 @@ public class Limits {
         this.min = min;
         this.max = max;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Limits limits = (Limits) o;
+
+        if (Double.compare(limits.min, min) != 0) return false;
+        return Double.compare(limits.max, max) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(min);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(max);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
